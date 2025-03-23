f6f68ee68a722e9a3557db5b573fa4fd6324404d
dukerupert <logan@fireflysoftware.dev>
2025-03-23 12:01:31 -0600

    Base card created

---
+++ src/lib/card/base.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	interface Base {
+		name: string;
+	}
+	interface Props {
+		data: Base;
+	}
+	let { data = { name: 'Base' } }: Props = $props();
+</script>
+
+<div
+	id="card"
+	class="relative h-full w-full border-t-1 border-r-2 border-b-4 border-l-1 border-black bg-white"
+>
+	<div class="absolute top-0 left-0 min-w-12 bg-black p-2 text-center text-sm text-white">
+		<h3>{data.name}</h3>
+	</div>
+	<div
+		class="grid h-full w-full grid-cols-1 grid-rows-1 items-center justify-items-center text-2xl"
+	>
+		Character name
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Grid, { GridItem } from 'svelte-grid-extended';
+	import BaseCard from '$lib/card/base.svelte';
+
+	const itemSize = { height: 40 };
+</script>
+
+<Grid {itemSize} collision="push" cols={10} class="min-h-screen">
+	<GridItem x={1} y={0} class="bg-blue-300"><form><input type="text" /></form></GridItem>
+	<GridItem x={3} y={3} w={4} class="bg-green-300">Hoy</GridItem>
+	<GridItem x={0} y={1} w={4}><BaseCard data={{ name: 'Name' }}></BaseCard></GridItem>
+</Grid>
